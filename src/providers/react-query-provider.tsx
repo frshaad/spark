adf920c4733368ac04cfbc111c3b1f61967d4475
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2026-01-03 13:55:18 +0330

    refactor(react-query): make it completely client-side

--- src/providers/react-query-provider.tsx
+++ src/providers/react-query-provider.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { getQueryClient } from '@/lib/get-query-client';
-import { QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function ReactQueryProvider({
@@ -9,10 +9,11 @@ export default function ReactQueryProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const queryClient = getQueryClient();
+  const queryClient = new QueryClient();
+  const [client] = useState(queryClient);
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={client}>
       {children}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
